997b3c5bf01432cc0b31fd81c41470391f4c7b4f
Andrei-George Lichi <lichiag21@gmail.com>
2025-05-10 02:35:10 +0200

    render model added

---
+++ app/components/RenderModel.jsx
@@ -0,0 +1,19 @@
+"use client"
+
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Environment} from '@react-three/drei'
+
+const RenderModel = ({ children }) => {
+  return (
+    <Canvas className="w-screen h-screen relative" camera={{ position: [0, 2, 10], fov: 50 }}>
+      <Suspense fallback={null}>
+        {children}
+        {}
+      </Suspense>
+      <Environment preset="lobby"/>
+    </Canvas>
+  )
+}
+
+export default RenderModel
