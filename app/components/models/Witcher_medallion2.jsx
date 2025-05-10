ec59bcde9143ba078259c42afd3c71c2879487cb
Andrei-George Lichi <lichiag21@gmail.com>
2025-05-10 02:35:31 +0200

    witcher logo jsx file created

---
+++ app/components/models/Witcher_medallion2.jsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { useFrame, useThree } from '@react-three/fiber'
+
+export default function Model(props) {
+  const { nodes, materials } = useGLTF("/models/witcher_medallion2.glb")
+  const modelRef = useRef()
+
+  useFrame((state) => {
+    const elapsed = state.clock.getElapsedTime()
+
+    modelRef.current.position.y = Math.sin(elapsed) * 0.2
+
+  })
+
+  return (
+    <group {...props} dispose={null} ref={modelRef}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Witcher_medalion_Material001_0.geometry}
+        material={materials['Material.001']}
+        scale={0.06}
+        position={[0, 0, 0]}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Witcher_medalion_Material001_0_1.geometry}
+        material={materials['Material.001']}
+        scale={0.06}
+        position={[0, 0, 0]}
+      />
+    </group>
+  )
+}
+
+useGLTF.preload("/models/witcher_medallion2.glb")
