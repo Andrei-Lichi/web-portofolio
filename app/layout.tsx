c2e8ab68a28e29a6bd8be22f4d7a0529cb5abeba
Andrei-George Lichi <lichiag21@gmail.com>
2025-05-11 21:59:12 +0200

    layout description changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Lichi's portofolio",
-  description: "A portofolio of projects",
+  description: "Web portofolio",
 };
 
 export default function RootLayout({
