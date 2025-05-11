aa60659608bfa502ad343b668eb72a3e6174fba2
Andrei-George Lichi <lichiag21@gmail.com>
2025-05-11 21:35:13 +0200

    added circular menu

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -1,9 +1,28 @@
 "use client"
+
 import React from 'react';
+import { listOfButtons } from '../../data/seed'
 
 const Navbar = () => {
+
+    const circleDegress = 360
+    const numberOfButtons =  circleDegress / listOfButtons.length;
+
     return (
-        <div>Navbar</div>
+        <div className='fixed h-screen flex items-center justify-center'>
+            <div className= 'flex items-center justify-inbetween relative'>
+                {   
+                    listOfButtons.map((button, index) => {
+                        const buttonPosition = (index * numberOfButtons * Math.PI) / 180;
+                        const circleRadius = 'calc(20vw - 1rem)';
+                        const x = `calc(${circleRadius} * ${Math.cos(buttonPosition)})`;
+                        const y = `calc(${circleRadius} * ${Math.sin(buttonPosition)})`;
+
+                        return <button key = {index} className='absolute' style={{transform: `translate(${x}, ${y})`}}> {button.label} </button>
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
